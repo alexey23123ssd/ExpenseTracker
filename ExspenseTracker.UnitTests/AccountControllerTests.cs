120205307f1bab3918e13090ca5bc72202412203
Viktorov Aleksey <yfcnz200608@gmail.com>
2024-04-22 19:36:40 +0300

    Add a tests of CreateAccount method for AccountController

--- ExspenseTracker.UnitTests/AccountControllerTests.cs
+++ ExspenseTracker.UnitTests/AccountControllerTests.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using BL.Models;
 using BL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Moq;
 using WebAPI.Controllers;
 namespace ExspenseTracker.UnitTests
@@ -10,25 +12,72 @@ namespace ExspenseTracker.UnitTests
         private IAccountService _accountService;
         private IMapper _mapper;
         private AccountController _accountController;
-        [SetUp]
-        public void Setup()
+
+
+        [Test]
+        public  void CreateAccount_ModelCreated_ModelStateIsValid()
         {
+            //Arrange
+            var account = new DAL.Models.Account()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+            };
+
             var accountService = new Mock<IAccountService>();
-            accountService.Setup(x => x.CreateAccountAsync(It.IsAny<Account>()));
-            _accountService = Mock.Of<IAccountService>();
-            _mapper = Mock.Of<IMapper>();
-            _accountController = new AccountController(_accountService,_mapper);
+            accountService.Setup(x => x.CreateAccountAsync(It.IsAny<Account>()))
+                .Returns((Task<ServiceDataResponse<Account>>)Task.CompletedTask)
+                .Verifiable();
+            var mapper = new Mock<IMapper>();
+            mapper.Setup(x => x.Map<Account>(It.IsAny<DAL.Models.Account>()));
+            _accountController = new AccountController(accountService.Object, mapper.Object);
+
+            //Act
+            var result = _accountController.CreateAccount(account);
+            //Assert
+            Assert.That();
+            Assert.Pass();
         }
 
         [Test]
-        public void CreateAccount_WithValidModel_ModelCreated()
+        public void CreateAccount_ReturnsBadRequest_ModelStateIsInvalid()
         {
             //Arrange
-            
+            var account = new DAL.Models.Account()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+            };
+
+            var accountService = new Mock<IAccountService>();
+            accountService.Setup(x => x.CreateAccountAsync(It.IsAny<Account>()))
+                .Returns((Task<ServiceDataResponse<Account>>)Task.CompletedTask)
+                .Verifiable();
+            var mapper = new Mock<IMapper>();
+            mapper.Setup(x => x.Map<Account>(It.IsAny<DAL.Models.Account>()));
+            _accountController = new AccountController(accountService.Object, mapper.Object);
+            _accountController.ModelState.AddModelError("Name", "Name is required");
             //Act
-            
+            var result = _accountController.CreateAccount(account);
             //Assert
-            Assert.Pass();
+            var badRequestResult = Assert.
+        }
+
+        [Test]
+        public void DeleteAccount_ModelDeleted_ModelStateIsValid()
+        {
+            //Arrange
+            Guid id = Guid.NewGuid();
+            var accountService = new Mock<IAccountService>();
+            accountService.Setup(x => x.DeleteAccountAsync(It.IsAny<Guid>()))
+                .Returns((Task<ServiceResponse>)Task.CompletedTask)
+                .Verifiable();
+            var mapper = new Mock<IMapper>();
+            _accountController = new AccountController(accountService.Object,mapper.Object);
+            //Act
+            var result = _accountController.DeleteAccount(id);
+            //Assert
+
         }
     }
 }
